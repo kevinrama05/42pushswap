9474c4c9565be39a52f674e1e424e615ae5d0152
vangjel.gramozi <vangjel.gramozi@learner.42.tech>
2025-12-14 18:32:56 +0100

    Implement stack and node operations.

---
+++ ps_node_utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_node_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgramozi <vgramozi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 18:30:04 by vgramozi          #+#    #+#             */
+/*   Updated: 2025/12/14 18:31:02 by vgramozi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_node	*ft_new_node(int value)
+{
+	t_node	*new;
+
+	new = (t_node *)malloc(sizeof(t_node));
+	if (!new)
+		ft_error_exit();
+	new->value = value;
+	new->index = -1;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_add_node_front(t_stack *stack, t_node *new)
+{
+	if (!stack || !new)
+		return ;
+	if (stack->size == 0)
+	{
+		stack->head = new;
+		stack->tail = new;
+	}
+	else
+	{
+		new->next = stack->head;
+		stack->head->prev = new;
+		stack->head = new;
+	}
+	stack->size++;
+}
+
+
+t_node	*ft_del_node_front(t_stack *stack)
+{
+	t_node	*temp;
+
+	if (!stack || stack->size == 0)
+		return (NULL);
+	temp = stack->head;
+	if (stack->size == 1)
+	{
+		stack->head = NULL;
+		stack->tail = NULL;
+	}
+	else
+	{
+		stack->head = temp->next;
+		stack->head->prev = NULL;
+		temp->next = NULL;
+	}
+	stack->size--;
+	return (temp);
+}
