167d99339c5b0fe4bf6e983a77edd270efa3d746
vangjel.gramozi <vangjel.gramozi@learner.42.tech>
2025-12-14 19:27:05 +0100

    Add arg parser and tuilc stack a

--- push_swap_utils.h
+++ push_swap_utils.h
@@ -6,7 +6,7 @@
 /*   By: vgramozi <vgramozi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 18:24:25 by vgramozi          #+#    #+#             */
-/*   Updated: 2025/12/14 18:53:20 by vgramozi         ###   ########.fr       */
+/*   Updated: 2025/12/14 19:19:28 by vgramozi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,30 @@
 
 # include "push_swap_structs.h"
 
+t_ps_counts	*ft_init_counts(void);
+t_stack		*ft_init_stack(char name);
+t_ps_data	*ft_init_data(void);
+void		ft_free_stack(t_stack *stack);
+void		ft_clean_exit(t_ps_data *data, int success_code);
+
 t_node		*ft_new_node(int value);
 void		ft_add_node_front(t_stack *stack, t_node *new);
 t_node		*ft_del_node_front(t_stack *stack);
 void		ft_add_node_back(t_stack *stack, t_node *new);
 t_node		*ft_del_node_back(t_stack *stack);
-void		ft_free_stack(t_stack *stack);
 
-t_ps_data	*ft_init_data(void);
-void		ft_clean_exit(t_ps_data *data, int success_code);
+char		**ft_parse_arguments(t_ps_data *data, int argc, char **argv);
+int			ft_is_strategy_selector(const char *arg);
+void		ft_set_strategy(t_ps_data *data, const char *arg);
+int			ft_get_num_count(int argc, char **argv);
+
+void		ft_assign_strategy_details(t_ps_data *data, const char *arg);
+int			ft_is_arg_number(const char *arg);
+void		ft_check_unrecognized_flag(const char *arg);
+char		**ft_collect_numbers(int argc, char **argv, int num_count,
+				t_ps_data *data);
+void		ft_error_exit(void);
+void		ft_build_stack_a(t_ps_data *data, char **num_args, int num_count);
+void		ft_is_sorted(t_ps_data *data);
 
 #endif
