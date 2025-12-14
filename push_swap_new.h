6ce12e0804f8ca1f3d2438b6df8f07b9793a7209
vangjel.gramozi <vangjel.gramozi@learner.42.tech>
2025-12-14 18:20:14 +0100

    Add separate header files for the needed operations.

---
+++ push_swap_new.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_new.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgramozi <vgramozi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 18:15:56 by vgramozi          #+#    #+#             */
+/*   Updated: 2025/12/14 18:16:19 by vgramozi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_NEW_H
+# define PUSH_SWAP_NEW_H
+
+# include "push_swap_error.h"
+# include "push_swap_ops.h"
+# include "push_swap_structs.h"
+# include "push_swap_utils.h"
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
